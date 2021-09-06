527c6ada3afa644fb2a0090267fbcc9794572435
elgnonvis <elgnonvis@gmail.com>
2021-09-06 07:01:44 +0100

    initiation of the project + timezone, internalization and generators config

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module AppInstagram
+  class Application < Rails::Application
+
+    config.time_zone = 'West Central Africa'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :fr
+    
+    config.load_defaults 5.2
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+  end
+end
